b6529e433d8eab540a5cfb7343772589ba900629
Cengiz Kuscu <cengizkuscu@gmail.com>
2023-10-25 16:12:26 +0300

    [ADD] upm package test

---
+++ Assets/ChefPlugins/Runtime/GoogleAds/GoogleAdsManager.cs
@@ -0,0 +1,12 @@
+using VContainer.Unity;
+
+namespace gs.chef.plugins.GoogleAds
+{
+    public class GoogleAdsManager : IInitializable
+    {
+        public void Initialize()
+        {
+            
+        }
+    }
+}
